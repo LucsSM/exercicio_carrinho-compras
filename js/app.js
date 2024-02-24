389ae4022dbdfb5407ec9fd8b67141fdcead583d
LucasSM <lucas.silvamarra@gmail.com>
2024-02-24 19:27:45 -0300

    inserir produto no carrinho de compras

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 function adicionar() {
+    //obter nome do produto, valor e preço
     let produto = document.getElementById("produto").value;
     let quantidadeProduto = document.getElementById("quantidade").value;
 
     let nomeProduto = produto.split("-")[0];
     let valorProduto = produto.split("R$")[1];
     let valorProdutoTotal = valorProduto * quantidadeProduto;
+
+    //inserir produto no carrinho de compras
+    let listaProdutos = document.getElementById("lista-produtos");
+    let produtoSelecionado = `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidadeProduto}</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+  </section>`;
+  listaProdutos.innerHTML = listaProdutos.innerHTML + produtoSelecionado;
 
     console.log(nomeProduto);
     console.log(valorProduto);
